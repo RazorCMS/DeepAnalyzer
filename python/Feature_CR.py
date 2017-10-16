7aac2a02dcab19b4233e59ad88490dee9f9eb55e
Thong <thongnguyen1292@gmail.com>
2017-10-16 04:50:57 +0200

    Work around a pyROOT bug that does not fill in the first histogram

--- python/Feature_CR.py
+++ python/Feature_CR.py
@@ -1,9 +1,18 @@
 import ROOT as rt
 import os
+import random
 
 rt.gROOT.SetBatch(True)
-
+rt.TH1.AddDirectory(rt.kFALSE)
 rt.gStyle.SetOptStat(0)
+rt.gStyle.SetLegendBorderSize(0)
+
+SAVEFILE = 'ControlRegionPlots.root'
+newfile = rt.TFile(SAVEFILE,"recreate")
+newfile.Close()
+
+SAVEDIR = '/eos/user/q/qnguyen/www/InclusiveFeature_CR_NoMTcut/'
+
 COLORS = {
         "WJets":rt.kRed+1, 
         "WJetsInv":rt.kRed+1, 
@@ -25,23 +34,27 @@ COLORS = {
         }
 
 def draw_plot(hist, tree, feature, sample):
-    #cr = rt.TFile("ControlRegionPlots.root","update")
+    cv = rt.TCanvas("cv","",700,600)
     lumi = "35.9"
     if sample is "Data": lumi = "1."
-    cv = rt.TCanvas("cv","",700,600)
-    tree.Draw(feature+">>"+hist.GetName(),"weight*"+lumi+"*leadingJetPt>100")
     hist.SetLineWidth(3)
-    #if sample is not 'Data': hist.SetFillColor(COLORS[sample])
-    hist.Draw()
-    save_dir = '/eos/user/q/qnguyen/www/InclusiveFeature_CR_NoMTcut/'
-    if not os.path.isdir(save_dir): os.makedirs(save_dir)
-    cv.SaveAs(save_dir+"/"+hist.GetName()+".png")
-    #hist.Write()
-    #cr.Close()
-    return hist
+    if sample is not 'Data': 
+        hist.SetFillColor(COLORS[sample])
+        hist.SetLineColor(COLORS[sample])
+    tree.Draw(feature+">>"+hist.GetName(),"weight*"+lumi+"*((abs(lep1Type) == 11 || abs(lep1Type) == 13) && lep1PassTight && ((abs(lep1Type) == 11 && lep1.Pt() > 30) || (abs(lep1Type) == 13 && lep1.Pt() > 25)))")
+    if not os.path.isdir(SAVEDIR): os.makedirs(SAVEDIR)
+    if hist.Integral() > 0:
+        cv.SaveAs(SAVEDIR+"/"+hist.GetName()+".png")
+    del cv
+
+def save_plot(hist, tree, feature, sample):
+    print hist.Integral()
+    cr = rt.TFile.Open(SAVEFILE,"update")
+    hist.Write()
+    cr.Close()
 
 SAMPLES = {}
-filedir = '/eos/cms/store/group/phys_susy/razor/Run2Analysis/RunTwoInclusiveControlRegion/2016/V3p15_4Oct2017_Inclusive/OneLeptonFull/'
+filedir = '/eos/cms/store/group/phys_susy/razor/Run2Analysis/RunTwoInclusiveControlRegion/2016/V3p15_13Oct2017_Inclusive/OneLeptonFull/'
 SAMPLES['WJets'] = {'file': filedir+"/InclusiveControlRegion_OneLeptonFull_SingleLeptonSkim_Razor2016_MoriondRereco_WJets_1pb_weighted.root"}
 SAMPLES['TTJets'] = {'file': filedir+"InclusiveControlRegion_OneLeptonFull_SingleLeptonSkim_Razor2016_MoriondRereco_TTJets_1pb_weighted.root"}
 SAMPLES['Other'] = {'file': filedir+"InclusiveControlRegion_OneLeptonFull_SingleLeptonSkim_Razor2016_MoriondRereco_Other_1pb_weighted.root"}
@@ -59,54 +72,63 @@ SAMPLES['DYJets']['test'] = filedir+'jobs/InclusiveControlRegion_OneLeptonFull_S
 SAMPLES['SingleTop']['test'] = filedir+'jobs/InclusiveControlRegion_OneLeptonFull_SingleLeptonSkim_Razor2016_MoriondRereco_ST_t-channel_antitop_4f_inclusiveDecays_13TeV-powhegV2-madspin-pythia8_TuneCUETP8M1.Job121of678.root'
 SAMPLES['Data']['test'] = filedir+'/jobs/InclusiveControlRegion_OneLeptonFull_SingleLeptonSkim_Razor2016_MoriondRereco_SingleElectron_2016B_03Feb2017.Job145of199.root'
 
+#print SAMPLES['Data']['test']
 
 for sample in SAMPLES:
-    #SAMPLES[sample]['tree'] = rt.TFile.Open(SAMPLES[sample]['file']).Get('InclusiveControlRegion')
+    SAMPLES[sample]['feature'] = {}
     _file = rt.TFile.Open(SAMPLES[sample]['file'])
     _tree = _file.Get('InclusiveControlRegion')
-    SAMPLES[sample]['Rsq'] =  rt.TH1F(sample+"_Rsq",";R^{2}",100,0,1.2)
-    SAMPLES[sample]['MR'] =  rt.TH1F(sample+"_MR",";M_{R}",100,0,2000)
-    SAMPLES[sample]['dPhiRazor'] =  rt.TH1F(sample+"_dPhiRazor",";#Delta#phi_{Hemispheres}",100,-3.4,3.4)
-    SAMPLES[sample]['dPhiMinJetMET'] =  rt.TH1F(sample+"_dPhiMinJetMET",";min(#Delta#phi_{jet,MET})",100,0,3.4)
-    SAMPLES[sample]['MT2'] =  rt.TH1F(sample+"_MT2",";M_{T2}",100,0,300)
-    SAMPLES[sample]['MHT'] =  rt.TH1F(sample+"_MHT",";H_{T}^{miss}",100,0,500)
-    SAMPLES[sample]['MET'] =  rt.TH1F(sample+"_MET",";E_{T}^{miss}",100,0,300)
-    SAMPLES[sample]['alphaT'] =  rt.TH1F(sample+"_alphaT",";#alpha_{T}",100,0,1.2)
-    SAMPLES[sample]['HT'] =  rt.TH1F(sample+"_HT",";H_{T}",100,0,1500)
-    SAMPLES[sample]['nJets'] =  rt.TH1F(sample+"_nJets",";n_{Jets}",10,0,10)
-    SAMPLES[sample]['leadingJetPt'] =  rt.TH1F(sample+"_leadingJetPt",";Leading Jet PT",100,80,400)
-    SAMPLES[sample]['subleadingJetPt'] =  rt.TH1F(sample+"_subleadingJetPt",";Subleading Jet PT",100,20,250)
-    
-    SAMPLES[sample]['Rsq'] = draw_plot(SAMPLES[sample]['Rsq'], _tree, "Rsq", sample) 
-    SAMPLES[sample]['MR'] = draw_plot(SAMPLES[sample]['MR'], _tree, "MR", sample) 
-    SAMPLES[sample]['MT2'] = draw_plot(SAMPLES[sample]['MT2'], _tree, "MT2", sample) 
-    SAMPLES[sample]['MHT'] = draw_plot(SAMPLES[sample]['MHT'], _tree, "MHT", sample) 
-    SAMPLES[sample]['MET'] = draw_plot(SAMPLES[sample]['MET'], _tree, "MET", sample) 
-    SAMPLES[sample]['alphaT'] = draw_plot(SAMPLES[sample]['alphaT'], _tree, "alphaT", sample) 
-    SAMPLES[sample]['HT'] = draw_plot(SAMPLES[sample]['HT'], _tree, "HT", sample) 
-    SAMPLES[sample]['nJets'] = draw_plot(SAMPLES[sample]['nJets'], _tree, "nSelectedJets", sample) 
-    SAMPLES[sample]['dPhiRazor'] = draw_plot(SAMPLES[sample]['dPhiRazor'], _tree, "dPhiRazor", sample) 
-    SAMPLES[sample]['dPhiMinJetMET'] = draw_plot(SAMPLES[sample]['dPhiMinJetMET'], _tree, "dPhiMinJetMET", sample) 
-    SAMPLES[sample]['leadingJetPt'] = draw_plot(SAMPLES[sample]['leadingJetPt'], _tree, "leadingJetPt", sample) 
-    SAMPLES[sample]['subleadingJetPt'] = draw_plot(SAMPLES[sample]['subleadingJetPt'], _tree, "subleadingJetPt", sample) 
+
+    SAMPLES[sample]['feature']['MR'] =  rt.TH1F(sample+"_MR",sample+";M_{R}",100,0,2000)
+    SAMPLES[sample]['feature']['Rsq'] =  rt.TH1F(sample+"_Rsq",sample+";R^{2}",100,0,1.2)
+    SAMPLES[sample]['feature']['dPhiMinJetMET'] =  rt.TH1F(sample+"_dPhiMinJetMET",sample+";min(#Delta#phi_{jet,MET})",100,0,3.4)
+    SAMPLES[sample]['feature']['MT2'] =  rt.TH1F(sample+"_MT2",sample+";M_{T2}",100,0,300)
+    SAMPLES[sample]['feature']['alphaT'] =  rt.TH1F(sample+"_alphaT",sample+";#alpha_{T}",100,0,1.2)
+    SAMPLES[sample]['feature']['MHT'] =  rt.TH1F(sample+"_MHT",sample+";H_{T}^{miss}",100,0,500)
+    SAMPLES[sample]['feature']['MET'] =  rt.TH1F(sample+"_MET",sample+";E_{T}^{miss}",100,0,300)
+    SAMPLES[sample]['feature']['HT'] =  rt.TH1F(sample+"_HT",sample+";H_{T}",100,0,1500)
+    SAMPLES[sample]['feature']['nSelectedJets'] =  rt.TH1F(sample+"_nSelectedJets",sample+";n_{Jets}",10,0,10)
+    SAMPLES[sample]['feature']['dPhiRazor'] =  rt.TH1F(sample+"_dPhiRazor",sample+";#Delta#phi_{Hemispheres}",100,-3.4,3.4)
+    SAMPLES[sample]['feature']['leadingJetPt'] =  rt.TH1F(sample+"_leadingJetPt",sample+";Leading Jet PT",100,80,400)
+    SAMPLES[sample]['feature']['subleadingJetPt'] =  rt.TH1F(sample+"_subleadingJetPt",sample+";Subleading Jet PT",100,20,250)
+  
+    features = list(SAMPLES[sample]['feature'])
+    #random.shuffle(features)
+    for feature in features:
+        _hist = SAMPLES[sample]['feature'][feature]
+        _hist.SetDirectory(rt.gROOT)
+        draw_plot(_hist, _tree, feature, sample) 
+        save_plot(_hist, _tree, feature, sample)
+
+    for feature in features:
+        _hist = SAMPLES[sample]['feature'][feature]
+        if _hist.Integral() == 0:
+            print "Redone {}_{} for a stupid pyROOT pointer/ownership problem that does not fill in the first histogram".format(sample,feature)
+            draw_plot(_hist, _tree, feature, sample) 
+            save_plot(_hist, _tree, feature, sample)
 
 STACKS = {}
-for feature in SAMPLES['Data']:
-    if feature is 'file': continue
+for feature in SAMPLES['Data']['feature']:
     STACKS[feature] = rt.THStack(feature+"_stack","")
 
+savefile = rt.TFile.Open(SAVEFILE,"read")
 for sample in SAMPLES:
-    for feature in SAMPLES['Data']:
-        if feature is 'file': continue
-        if sample is not 'Data': STACKS[feature].Add(SAMPLES[sample][feature])
+    for feature in SAMPLES['Data']['feature']:
+        if sample is not 'Data': 
+            _hist = savefile.Get(sample+"_"+feature)
+            _hist.SetFillColor(COLORS[sample])
+            _hist.SetLineColor(COLORS[sample])
+            STACKS[feature].Add(_hist)
 
-for feature in SAMPLES['Data']:
-    if feature is 'file': continue
+for feature in SAMPLES['Data']['feature']:
     cv = rt.TCanvas("cv","",700,600)
-    STACKS[feature].Draw()
-    SAMPLES['Data'][feature].Draw("same")
-    save_dir = '/eos/user/q/qnguyen/www/InclusiveFeature_CR/'
-    if not os.path.isdir(save_dir): os.makedirs(save_dir)
-    cv.SaveAs(save_dir+"/"+feature+".png")
-
+    STACKS[feature].Draw("hist")
+    _hist = savefile.Get("Data_"+feature)
+    _hist.SetLineWidth(2)
+    _hist.SetMarkerStyle(rt.kFullCircle);
+    _hist.Draw("E1 SAME")
+    leg = cv.BuildLegend(0.5,0.7,0.85,0.88)
+    leg.SetNColumns(2)
+    if not os.path.isdir(SAVEDIR): os.makedirs(SAVEDIR)
+    cv.SaveAs(SAVEDIR+"/"+feature+".png")
 

4344851ed09978212f0defbe8cfd845c73b0d15c
Thong <thongnguyen1292@gmail.com>
2017-10-26 02:20:58 -0700

    Use weight as default and save output files to corresponding directories

--- keras/training.py
+++ keras/training.py
@@ -10,7 +10,6 @@ import os
 
 DATA_DIR = '/bigdata/shared/analysis/'
 #DATA_DIR = '/home/ubuntu/data/'
-SCALER = 'scaler.pkl'
 
 # Convert to regular numpy arrays
 def to_regular_array(struct_array):
@@ -144,11 +143,11 @@ def load_dataset(location, load_type = 0, train_size = 0):
     if train_size==0:
         _x = dat[:,2:]
         _y = dat[:,0].astype(int)
-        _weight = dat[:,1]*1e6
+        _weight = dat[:,1]
     else:
         _x = dat[0:train_size,2:]
         _y = dat[0:train_size,0]
-        _weight = dat[0:train_size,1]*1e6
+        _weight = dat[0:train_size,1]
     has_nan(_x)
     has_nan(_y)
     has_nan(_weight)
@@ -162,13 +161,17 @@ def get_class_weight(label):
     print ("class_weight: {}".format(class_weight))
     return class_weight
 
-def scale_fit(x_train):
+def scale_fit(x_train, sample_size=0):
     scaler = preprocessing.MinMaxScaler(feature_range=(0, 1)).fit(x_train)
-    joblib.dump(scaler, SCALER)
-    print ("Saving scaler information to {}".format(SCALER))
+    if not os.path.isdir('Scaler'):
+        os.makedirs('Scaler')
+    scaler_file = "Scaler/scaler_{}.pkl".format(sample_size)
+    joblib.dump(scaler, scaler_file)
+    print ("Saving scaler information to {}".format(scaler_file))
 
-def scale_dataset(x_train):
-    scaler = joblib.load(SCALER)
+def scale_dataset(x_train, sample_size=0):
+    scaler_file = "Scaler/scaler_{}.pkl".format(sample_size)
+    scaler = joblib.load(scaler_file)
     _x_train = scaler.transform(x_train)
     return _x_train
 
@@ -179,19 +182,15 @@ def create_model():
     # Training with a simple FFNN
     i = Input(shape=(14,))
     layer = Dense(100, activation = 'relu')(i)
-    #layer = Dropout(0.1)(layer)
     layer = Dense(30, activation = 'relu')(layer)
-    #layer = Dropout(0.2)(layer)
     layer = Dense(10, activation = 'relu')(layer)
-    #layer = Dropout(0.5)(layer)
     o = Dense(2, activation = 'softmax')(layer)
-    #o = Dense(1, activation=None)(layer)
 
     model = Model(i,o)
     model.summary()
     return model
 
-def training(train_size = 0, use_weight=False):
+def training(train_size = 0, not_use_weight=False, label='Default'):
     print ("Loading data...")
     x_train, y_train, weight_train = load_dataset(DATA_DIR+"/CombinedDataset_Balanced.h5",0,train_size = train_size)
     x_val, y_val, weight_val = load_dataset(DATA_DIR+"/CombinedDataset_Balanced.h5",1,train_size=train_size)
@@ -209,82 +208,112 @@ def training(train_size = 0, use_weight=False):
     has_nan(x_train, "unscaled training")
     if not np.isfinite(x_train).all():
         print ("Unscaled training probably contains inf")
-    scale_fit(x_train)
-    x_train = scale_dataset(x_train)
-    x_val = scale_dataset(x_val)
+    scale_fit(x_train, train_size)
+    x_train = scale_dataset(x_train, train_size)
+    x_val = scale_dataset(x_val, train_size)
     has_nan(x_train, "scaled training")
     has_nan(y_val, "scaled validation")
 
+    if not os.path.isdir('ScaledInput'):
+        os.makedirs('ScaledInput')
     # Save scaled training input to file
-    train_ds = h5py.File("TrainingDataset%d.h5"%(train_size),"w")
+    train_ds = h5py.File("ScaledInput/TrainingDataset%d.h5"%(train_size),"w")
     train_ds['x'] = x_train
     train_ds['y'] = y_train
     train_ds['w'] = weight_train
     train_ds.close()
-    print ("Write to TrainingDataset%d.h5"%(train_size))
+    print ("Write to ScaledInput/TrainingDataset%d.h5"%(train_size))
+    
+    # Save scaled val input to file
+    val_ds = h5py.File("ScaledInput/ValidationDataset%d.h5"%(train_size),"w")
+    val_ds['x'] = x_val
+    val_ds['y'] = y_val
+    val_ds['w'] = weight_val
+    val_ds.close()
+    print ("Write to ScaledInput/ValidationDataset%d.h5"%(train_size))
     
     class_weight = get_class_weight(y_train)
 
     model = create_model()
     from keras import optimizers
-    model.compile(optimizer = optimizers.Adam(lr=1e-3), loss = 'binary_crossentropy', metrics=['accuracy'])
-   
-    if (use_weight):
-        val_tuple = (x_val, y_val, weight_val)
-        sample_weight = weight_train
-    else:
+    model.compile(optimizer = optimizers.Nadam(), loss = 'binary_crossentropy', metrics=['accuracy'])
+    
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    print ("Write to model.json")
+
+    if (not_use_weight):
         val_tuple = (x_val, y_val)
         sample_weight = None
+    else:
+        val_tuple = (x_val, y_val, weight_val)
+        sample_weight = weight_train
     
     from keras.callbacks import ModelCheckpoint,EarlyStopping,ReduceLROnPlateau
+    if not os.path.isdir('CheckPoint'):
+        os.makedirs('CheckPoint')
     hist = model.fit(x_train, y_train,
             validation_data = val_tuple,
             epochs = 100,
             batch_size = 128,
             class_weight = class_weight,
             sample_weight = sample_weight,
-            callbacks = [ModelCheckpoint(filepath='CheckPoint%d.h5'%(train_size), verbose = 1, save_best_only=True), ReduceLROnPlateau(patience = 4, factor = 0.5, verbose = 1, min_lr=1e-7), EarlyStopping(patience = 10)],
+            callbacks = [ModelCheckpoint(filepath='CheckPoint/CheckPoint%d_%s.h5'%(train_size,label), verbose = 1, 
+                save_best_only=True), 
+                ReduceLROnPlateau(patience = 4, factor = 0.5, verbose = 1, min_lr=1e-7), 
+                EarlyStopping(patience = 10)],
             )
 
-    histfile = 'history%d.sav'%(train_size)
+    if not os.path.isdir('History'):
+        os.makedirs('History')
+    histfile = 'History/history%d_%s.sav'%(train_size, label)
     pickle.dump(hist.history, open(histfile,'wb'))
-    print ("Save history to history%d.sav"%(train_size))
+    print ("Save history to History/history%d_%s.sav"%(train_size, label))
 
     val_pred = model.predict(x_val)
     val_label = np.argmax(y_val, axis=1)
 
-    val_result = h5py.File("ValidationResult%d.h5"%(train_size),'w')
+    if not os.path.isdir('Result'):
+        os.makedirs('Result')
+    val_result = h5py.File("Result/ValidationResult{size}_{label}.h5".format(size=train_size, label=label),'w')
     val_result.create_dataset("Prediction", data = val_pred)
     val_result.create_dataset("Truth", data = val_label)
     val_result.create_dataset("Weight", data = weight_val)
-    print ("Save result to ValidationResult%d.h5"%(train_size))
+    print ("Save result to Result/ValidationResult{size}_{label}.h5".format(size=train_size, label=label))
     val_result.close()
 
-def testing(sample_size = 0, use_weight=False):
-    print ("Loading the model checkpoint...")
-    x_test, y_test, weight_test = load_dataset(DATA_DIR+"/CombinedDataset_Balanced.h5",2,train_size = sample_size)
-    x_test = scale_dataset(x_test)
+def testing(sample_size = 0, label='Default'):
 
     x_train, y_train, weight_train = load_dataset(DATA_DIR+"/CombinedDataset_Balanced.h5",0,train_size = sample_size)
-    x_train = scale_dataset(x_train)
+    x_train = remove_outlier(x_train)
+    x_train = scale_dataset(x_train, sample_size)
+
+    x_test, y_test, weight_test = load_dataset(DATA_DIR+"/CombinedDataset_Balanced.h5",2,train_size = sample_size)
+    x_test = remove_outlier(x_test)
+    x_test = scale_dataset(x_test, sample_size)
 
     from keras.models import load_model
-    model = load_model('CheckPoint%d.h5'%(sample_size))
+    print ("Loading the model checkpoint: CheckPoint/CheckPoint%d_%s.h5"%(sample_size,label))
+    model = load_model('CheckPoint/CheckPoint%d_%s.h5'%(sample_size, label))
     test_pred = model.predict(x_test)
     train_pred = model.predict(x_train)
 
-    test_result = h5py.File("TestResult%d.h5"%(sample_size),"w")
+    if not os.path.isdir('Result'):
+        os.makedirs('Result')
+    test_result = h5py.File("Result/TestResult%d_%s.h5"%(sample_size, label),"w")
     test_result.create_dataset("Prediction",data=test_pred)
     test_result.create_dataset("Truth",data=y_test)
     test_result.create_dataset("Weight",data=weight_test)
-    print("Save to TestResult%d.h5"%(sample_size))
+    print("Save to TestResult%d_%s.h5"%(sample_size, label))
     test_result.close()
 
-    train_result = h5py.File("TrainResult%d.h5"%(sample_size),"w")
+    train_result = h5py.File("Result/TrainResult%d_%s.h5"%(sample_size, label),"w")
     train_result.create_dataset("Prediction",data=train_pred)
     train_result.create_dataset("Truth",data=y_train)
     train_result.create_dataset("Weight",data=weight_train)
-    print("Save to TrainResult%d.h5"%(sample_size))
+    print("Save to TrainResult%d_%s.h5"%(sample_size, label))
     train_result.close()
 
 if __name__ == "__main__":
@@ -293,23 +322,21 @@ if __name__ == "__main__":
     parser.add_argument('-t','--test', action='store_true', help='Test on validation set')
     parser.add_argument('-s','--sample', type=int, default=0, help='Use a small sample for training and validation')
     parser.add_argument('-d','--device', default="0", help='GPU device to use')
-    parser.add_argument('-w','--weight', action='store_true', help='Use sample weights')
-
-    # TODO: When using sample weight, the prediction probability should be weighted as well.
-    # TODO: if using small sample, save output to a different file
+    parser.add_argument('-nw','--noweight', action='store_true', help='Not use sample weights')
+    parser.add_argument('-l','--label', default='', help='Label for benchmark study')
 
     args = parser.parse_args()
 
     print ("Using GPU ",args.device)
-    if args.weight: print ("Using sample weight")
+    if args.noweight: print ("Not using sample weight")
     os.environ["CUDA_VISIBLE_DEVICES"]=args.device
 
 
     if args.create:
         create_dataset()
     if args.test:
-        testing(args.sample, use_weight = args.weight)
+        testing(args.sample, label=args.label)
     else:
-        training(args.sample, use_weight = args.weight)
+        training(args.sample, not_use_weight = args.noweight, label=args.label)
         
 

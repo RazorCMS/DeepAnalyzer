abdeba59d221b4227ccd313a2d5c4efc8ad3e798
Thong <thongnguyen1292@gmail.com>
2018-02-14 22:04:21 +0100

    Add option to merge splitted transformed file

---
+++ python/mergeH5.py
@@ -0,0 +1,34 @@
+import h5py
+from argparse import ArgumentParser
+import glob
+import sys
+import numpy as np
+
+parser = ArgumentParser()
+parser.add_argument('sample',help='Sample to merge')
+
+args = parser.parse_args()
+
+samples = glob.glob(args.sample+'_*.h5')
+
+if len(samples) == 0: sys.exit("No files to merge")
+print(samples)
+merge = None
+for sample in sorted(samples):
+    print ("Processing {}".format(sample))
+    with h5py.File(sample,'r') as infile:
+        data = infile['Data'][:]
+        #print("Shape {}".format(data.shape))
+        if merge == None:
+            merge = np.copy(data)
+        else:
+            merge = np.hstack((merge, data))
+            #print("Merged shape {}".format(merge.shape))
+with h5py.File(args.sample,'w') as outfile:
+    outfile['Data'] = merge
+    print("Save to {}".format(args.sample))
+
+
+
+
+        
